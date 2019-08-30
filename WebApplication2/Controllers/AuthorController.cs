4a4f4f5599e574b7bf2d5b2aa71ca658f1389bca
seda8888-92 <seda8888-92@mail.ru>
2019-08-30 17:24:14 +0600

    addBooks

---
+++ WebApplication2/Controllers/AuthorController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using WebApplication2;
+
+namespace WebApplication1.Controllers
+{
+    public class AuthorController : Controller
+    {
+        // GET: Author
+        public ActionResult Index()
+        {
+            List<Authors> authors;
+
+            using (Model1 db = new Model1())
+            {
+                authors = db.Authors.ToList();
+            }
+
+            return View(authors);
+        }
+
+
+
+        [HttpGet]
+        public ActionResult CreateOrEdit(int? id)
+        {
+            Authors author = new Authors();
+
+            if (id != null)
+                using (Model1 db = new Model1())
+                {
+                    author = db.Authors.Where(x => x.Id == id).FirstOrDefault();
+                }
+            return View(author);
+        }
+
+
+        [HttpPost]
+        public ActionResult CreateOrEdit(Authors author)
+        {
+
+            using (Model1 db = new Model1())
+            {
+                if (author.Id != 0)
+                {
+                    var oldAuthor = db.Authors.Where(x => x.Id == author.Id).FirstOrDefault();
+                    oldAuthor.FirstName = author.FirstName;
+                    oldAuthor.LastName = author.LastName;
+                    db.SaveChanges();
+                }
+                else
+                {
+                    db.Authors.Add(author);
+                    db.SaveChanges();
+                }
+
+            }
+
+            return RedirectToActionPermanent("Index", "Author");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (Model1 db = new Model1())
+            {
+                var author = db.Authors.Where(a => a.Id == id).FirstOrDefault();
+                db.Authors.Remove(author);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index", "Author");
+        }
+    }
+}
---
+++ WebApplication2/Controllers/BookController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2;
+
+namespace WebApplication1.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Author
+        public ActionResult Index()
+        {
+            List<Books> books;
+
+            using (Model1 db = new Model1())
+            {
+                books = db.Books.ToList();
+            }
+
+            return View(books);
+        }
+
+
+
+        [HttpGet]
+        public ActionResult CreateOrEdit(int? id)
+        {
+            Books book = new Books();
+
+            if (id != null)
+                using (Model1 db = new Model1())
+                {
+                    book = db.Books.Where(x => x.Id == id).FirstOrDefault();
+                }
+            return View(book);
+        }
+
+
+        [HttpPost]
+        public ActionResult CreateOrEdit(Books book)
+        {
+
+            using (Model1 db = new Model1())
+            {
+                if (book.Id != 0)
+                {
+                    var oldBook = db.Books.Where(x => x.Id == book.Id).FirstOrDefault();
+                    oldBook.Title = book.Title;
+                    oldBook.Price = book.Price;
+                    oldBook.Pages = book.Pages;
+                    db.SaveChanges();
+                }
+                else
+                {
+                    db.Books.Add(book);
+                    db.SaveChanges();
+                }
+
+            }
+
+            return RedirectToActionPermanent("Index", "Book");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (Model1 db = new Model1())
+            {
+                var book = db.Books.Where(a => a.Id == id).FirstOrDefault();
+                db.Books.Remove(book);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index", "Book");
+        }
+    }
+}
