27b986a8c078264e85c66d22ee28df0b01a715b7
seda8888-92 <seda8888-92@mail.ru>
2019-08-30 18:22:55 +0600

    addOrders

---
+++ WebApplication2/Controllers/OrderController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Entities;
+
+namespace WebApplication2.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            List<Orders> orders;
+
+            using (Model1 db = new Model1())
+            {
+                orders = db.Orders.ToList();
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult EditOrCreateOrder(int? id)
+        {
+            Orders order = new Orders();
+
+
+            if (id != null)
+                using (Model1 db = new Model1())
+                {
+                    order = db.Orders.Where(x => x.Id == id).FirstOrDefault();
+                }
+
+            return View(order);
+        }
+
+        [HttpPost]
+        public ActionResult EditOrCreateOrder(Orders order)
+        {
+            using (Model1 db = new Model1())
+            {
+                if (order.Id != 0)
+                {
+                    var Oldorder = db.Orders.Where(x => x.Id == order.Id).FirstOrDefault();
+                    order.BookId = Oldorder.BookId;
+                    order.UserId = Oldorder.UserId;
+                }
+
+                else
+                {
+                    db.Orders.Add(order);
+
+                }
+                db.SaveChanges();
+            }
+
+            return RedirectToActionPermanent("Index", "Order");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (Model1 db = new Model1())
+            {
+                var order = db.Orders.Where(x => x.Id == id).FirstOrDefault();
+                db.Orders.Remove(order);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index", "Order");
+        }
+    }
+}
---
+++ WebApplication2/Entities/Orders.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Entities
+{
+    public class Orders
+    {
+        public int Id { get; set; }
+
+
+        public int BookId { get; set; }
+
+
+        public int UserId { get; set; }
+    }
+}
