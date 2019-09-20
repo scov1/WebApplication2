d1f1ae21b6f03d179bf074eb95da91362788ff12
seda8888-92 <seda8888-92@mail.ru>
2019-09-20 17:27:09 +0600

    addDropDawnList

--- WebApplication2/Entities/Orders.cs
+++ WebApplication2/Entities/Orders.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -9,14 +11,19 @@ namespace WebApplication2.Entities
     {
         public int Id { get; set; }
 
-
+        [Required]
+        [ForeignKey("Books")]
         public int BookId { get; set; }
 
-
+        [Required]
+        [ForeignKey("Users")]
         public int UserId { get; set; }
 
         public string AuthorName { get; set; }
 
         public string BookName { get; set; }
+
+        public Users Users { get; set; }
+        public Books Books { get; set; }
     }
 }
